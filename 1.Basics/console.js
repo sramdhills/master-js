51f9c87fbfef3d7ad8051b930bcdd8147cd97a76
D Hills Santillan <dhillsbest2000@gmail.com>
2023-12-30 04:48:44 +0800

    add exercise.js file and application learning

---
+++ 1.Basics/console.js
@@ -0,0 +1,25 @@
+//Variables
+//(reserved keyword)(variable name) (type of value)
+//declared variable
+let name;
+
+//assign value
+let lastname;
+lastname = "santillan";
+console.log(lastname);
+
+//conventions of writing a variable
+//do not write name as reserved keyword
+//no spaces in name variable
+//nospecial caharacters
+let theVariable = "variable";
+console.log(theVariable);
+
+//UPPERCASE
+let ALLCAPSLOCKNAME = "stringvalue";
+//under_score
+
+//lowerUpper
+let lowerUpper = 1234;
+lowerUpper = "changed value";
+console.log(lowerUpper);
