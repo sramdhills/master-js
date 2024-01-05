2214f524b0b49d5a2a09dca37628cba0c99ebe4b
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-05 22:59:41 +0800

    String methods

--- 1.Basics/exercise2.js
+++ 1.Basics/exercise2.js
@@ -52,16 +52,53 @@ console.log(10 != 10);
 console.log(10 == '10');
 
 // CHALLENGE Comparison Operators and Equality Operators
-let firstFaveNumb  = 9
-let secondFavNumb  = 3
+// let firstFaveNumb  = 9
+// let secondFavNumb  = 3
 
-console.log(firstFaveNumb > secondFavNumb);
-console.log(firstFaveNumb < secondFavNumb);
-console.log(firstFaveNumb >= secondFavNumb);
-console.log(firstFaveNumb <= secondFavNumb);
+// console.log(firstFaveNumb > secondFavNumb);
+// console.log(firstFaveNumb < secondFavNumb);
+// console.log(firstFaveNumb >= secondFavNumb);
+// console.log(firstFaveNumb <= secondFavNumb);
 
-console.log(firstFaveNumb === secondFavNumb);
-console.log(firstFaveNumb == secondFavNumb);
-console.log(firstFaveNumb !== secondFavNumb);
-console.log(firstFaveNumb != secondFavNumb);
+// console.log(firstFaveNumb === secondFavNumb);
+// console.log(firstFaveNumb == secondFavNumb);
+// console.log(firstFaveNumb !== secondFavNumb);
+// console.log(firstFaveNumb != secondFavNumb);
+
+//Strings
+let firstName = "          D Hills         "
+let secondName  = 'Determination'
+let fullName1 = firstName + ' ' + secondName //string concatenation
+
+console.log(fullName1);
+
+// 1. Concatenation
+let fullName = firstName.concat(secondName) //concat method
+
+console.log(fullName);
+
+// 2. Append
+
+firstName += " something else"
+console.log(firstName);
+
+// 3. Length
+console.log(firstName.length);
+
+// 4. Cases
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+
+// 5. Slice - returns section
+console.log(firstName.slice(1,3));
+
+// 6. Split & Join
+console.log(firstName.split(' ').join(' '));
+
+// 7. Includes
+console.log(firstName.includes("h"));
+
+// 8. Trim 
+console.log(firstName.trim());
+
 
