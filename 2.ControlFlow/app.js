7994364a6d0b5ffd28a574b6873909060e966da2
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-08 04:32:16 +0800

    Challenge Switch Statements

--- 2.ControlFlow/app.js
+++ 2.ControlFlow/app.js
@@ -73,3 +73,19 @@ switch (day) {
 }
 
 //CHALLENGE SWITCH STATEMENTS
+
+let fruit = 'Banana'
+
+switch (fruit) {
+    case "Banana":
+        console.log("Banana is good!");
+        break;
+    case "Orange":
+        console.log("I am not a fan of orange");
+        break;
+    case "Apple":
+        console.log("How you like them apples?");
+        break;
+    default:
+        console.log("I have never heard of that fruit");
+}
