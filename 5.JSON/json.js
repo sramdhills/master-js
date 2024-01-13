c4f8a148c83c8c31069b47ede7bdd3c226738f00
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-13 18:20:40 +0800

    JSON

---
+++ 5.JSON/json.js
@@ -0,0 +1,13 @@
+//JSON - Javascript Object Notation(used in transmitting data from server)
+
+//JSON 
+const person = {
+    "userName": "hillsonmars",
+    "age": 23,
+    "email": "dhills@gmail.com",
+    "isSubscribed": true,
+    "hobbies": ["reading", "gym", "coding"],
+    "address": {
+        "city": "CDO",
+    },
+}
