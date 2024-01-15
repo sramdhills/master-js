9f81a95270f8d1664964c9c29899ebf41270b91e
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-15 23:39:49 +0800

    Template Strings/literals

--- 1,BeginnerJS/6.DATEandTIME/setIntervalTimeout.js
+++ 1,BeginnerJS/6.DATEandTIME/setIntervalTimeout.js
@@ -1,15 +1,15 @@
 //setInterval - repeat execute a function block code
 //in a limited interval.
 
-// setInterval(
-//   () => console.log(`This function will e executed every 3 seconds`),
-//   3000
-// );
+setInterval(
+  () => console.log(`This function will e executed every 3 seconds`),
+  3000
+);
 
-// setInterval(
-//   () => console.log(`This function is executed every 1 second`),
-//   1000
-// );
+setInterval(
+  () => console.log(`This function is executed every 1 second`),
+  1000
+);
 
 // setInterval(() => console.log(`This function executes every 2 seconds`), 2000);
 
--- 2.IntermediateJS/1.TemplateStrings.js
+++ 2.IntermediateJS/1.TemplateStrings.js
@@ -2,4 +2,39 @@
 // strings with embedded expressions.
 // `` uses backticks
 
-console.log(`Hello,`);
+console.log(`This is some
+random text`);
+
+//expressions inside the template literals
+console.log(`${2 + 2}`);
+
+const firstName = "Discipline"
+const lastName = "Coding"
+
+console.log(`My name is: ${firstName} ${lastName}`);
+
+console.log(`${ ['one', 'two', 'three'] }`);
+
+
+//render a js expressions, arrays, variables. in template literals.
+
+function info() {
+    return `My name is D for Discipline & I am 24 years old`
+}
+
+console.log(`${info()}`);
+
+//use template literals 
+
+//CHALLENGE TEMPLATE LITERALS
+
+console.log(`The quick
+brown fox
+jumps over
+the lazy dog`);
+
+
+const growth = "Determination"
+const mindset = "is in my Root"
+
+console.log(`My quote for today is "${growth} ${mindset}"`);
