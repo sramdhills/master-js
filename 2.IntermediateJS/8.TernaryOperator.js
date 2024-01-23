c6a2ac93d732e5ae2fafb199bd4f704e8af42e93
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-23 21:06:45 +0800

    Ternary (condition) ? : operator

--- 2.IntermediateJS/8.TernaryOperator.js
+++ 2.IntermediateJS/8.TernaryOperator.js
@@ -1,2 +1,31 @@
 //Ternary Operator
-//concise way of writinf conditional expressions.
+//concise way of writinf a simple conditional expressions.
+
+//condition ? exprIFTrue : exprIfFalse
+let password = 2
+function passwordChecker(ps) {
+//     if (ps === 8) {
+//         return `Strong password`
+//     } else {
+//         return `Password should be 8 characters`
+//     }
+// }
+return ps ===8 ? `Strong Password` : `Password should be 8 characters`
+
+}
+const res = passwordChecker(8)
+console.log(res);
+
+
+const age = 15
+const isAdult = age >= 18 ? `Adult` : `Not an Adult`
+console.log(isAdult);
+
+//CHALLENGE
+const money = 0
+const haveMoney = money > 0 ? `Buy Products` : `They should bring money`
+console.log(haveMoney);
+
+const personMoney = true
+const ifMoney = money === true ? `Buy products dude` : `Bring your dough!`
+console.log(ifMoney);
