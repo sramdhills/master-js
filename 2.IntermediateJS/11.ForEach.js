46f8d0c17051fa0db3a5198f1cb6e3a3c7083e21
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-27 01:29:08 +0800

    studing forEach helper

---
+++ 2.IntermediateJS/11.ForEach.js
@@ -0,0 +1,4 @@
+//forEach helper we  pass in anonymous callback function
+//This function gets called one time for every element in the array
+//Whatever is inside the function that logic happens.
+
