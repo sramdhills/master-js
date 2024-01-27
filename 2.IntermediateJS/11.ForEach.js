89dd8f2ec5965132d7081964b3ed56b713e54b55
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-27 18:14:28 +0800

    forEach function performs a function to each element in the array

--- 2.IntermediateJS/11.ForEach.js
+++ 2.IntermediateJS/11.ForEach.js
@@ -2,3 +2,22 @@
 //This function gets called one time for every element in the array
 //Whatever is inside the function that logic happens.
 
+const colors = ['teal', 'blue', 'red', 'green']
+
+colors.forEach(col => console.log(`${col} is the color`));
+
+
+//next example
+
+//challenge forEach
+
+const arrNum = [1, 3, 5, 62, 566, 233]
+
+let sum = 0//sum variable
+
+function adder(number) {
+    sum += number
+}
+
+arrNum.forEach(adder)
+console.log(sum);
