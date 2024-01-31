ff09239d76c4b67341fe4bf84fce2d75555656fc
Sram D Hills <dhillsbest2000@gmail.com>
2024-01-31 12:41:12 +0800

    Map Method vs forEach Method

--- 2.IntermediateJS/12.MapMethod.js
+++ 2.IntermediateJS/12.MapMethod.js
@@ -9,6 +9,28 @@ console.log(double);
 //map method modifies an array and returns into a new variable array
 //forEach performs function on each element, but doesn't return anything
 
-  
+let peoples = [
+    {firstName: "Sram D Hills", lastName: "Santillan"},
+    {firstName: "Ysheen", lastName: "Laguna"},
+]
+
+const results = peoples.map(person => {
+    return [person.firstName, person.lastName]
+})
+
+console.log(results);
+
+//Challenge ARray Map Method
+
+let randomNum = [2, 3, 4, 6, 7, 1, 3]
+    const product = randomNum.map(num => num * 10)
+    console.log(product);
 
 
+//using forEach
+let allProduct = 1
+ function multiplyFunct(num) { //the parameter is the elements of the array
+    allProduct *= num
+ }
+ randomNum.forEach(multiplyFunct)
+ console.log(allProduct)
