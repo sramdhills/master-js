025f3262e95fb1660b7d803aa263e887ff44fb94
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-04 00:13:26 +0800

    Map Method Challenge

--- 2.IntermediateJS/17.MapMethod.js
+++ 2.IntermediateJS/17.MapMethod.js
@@ -12,37 +12,70 @@
 
 
 
-const map = new Map()
+// const map = new Map()
 
-const keyOne = "string"
-const keyTwo = {}
-const keyThree =  function() {} 
+// const keyOne = "string"
+// const keyTwo = {}
+// const keyThree =  function() {} 
 
-map.set(keyOne, "Value of key one")
-map.set(keyTwo, "Value of key two")
-map.set(keyThree, "Value of key three")
+// map.set(keyOne, "Value of key one")
+// map.set(keyTwo, "Value of key two")
+// map.set(keyThree, "Value of key three")
 
-console.log(map.keys())
-console.log(map.values());
-console.log(map.delete(keyTwo));
-console.log(map.size);
+// console.log(map.keys())
+// console.log(map.values());
+// console.log(map.delete(keyTwo));
+// console.log(map.size);
+// console.log(map.get(KeyOne))
 
-console.log(map)
+// console.log(map)
 
 
 
 //
 
 //getting the key and values
+// for (let [key, value] of map) {
+//     console.log(`${key} -- ${value}`);
+// }
+// //getting the keys
+// for (let key of map.keys()) {
+//     console.log(key);
+// }
+
+// //getting the values
+// for (let value of map.values()) {
+//     console.log(value);
+// }
+
+
+//CHALLENGE Map
+
+const map = new Map
+
+// const keyOne = "a"
+// const keyTwo = "b"
+// const keyThree = "c"
+
+map.set("a", 1)
+map.set("b", 2)
+map.set("c", 3)
+
+console.log(map.get("a"));
+console.log(map.size);
+map.delete("b"); //deletes the property "b"
+console.log(map.size);
+
+//getting the key and values
+
 for (let [key, value] of map) {
-    console.log(`${key} -- ${value}`);
+    console.log(`${key} = ${value}`);
 }
-//getting the keys
+
 for (let key of map.keys()) {
     console.log(key);
 }
 
-//getting the values
 for (let value of map.values()) {
     console.log(value);
 }
