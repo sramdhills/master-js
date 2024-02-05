5c0692007f051ee5ca84f9fd69071b75bcaf6b7c
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-06 00:07:23 +0800

    Set Data Structures

---
+++ 2.IntermediateJS/18.SetDataStructures.js
@@ -0,0 +1,7 @@
+//Set is a built-in data structure introduced in ES6 that represents a collention
+//of unique values. Unlike arrats, Set allows you to stoe only unique values.
+//which means duplicate values are automatically removed. Each value can occur ONCE
+
+
+//Set objects are useful when you need to store a list of unique elements and quickly 
+//check for the existence of a specific value.
