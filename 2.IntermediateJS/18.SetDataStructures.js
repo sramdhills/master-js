060bbaa4d46b6faf01d2b8f9aca607c662bea191
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-06 23:13:40 +0800

    Set Set Setttt

--- 2.IntermediateJS/18.SetDataStructures.js
+++ 2.IntermediateJS/18.SetDataStructures.js
@@ -1,7 +1,41 @@
 //Set is a built-in data structure introduced in ES6 that represents a collention
-//of unique values. Unlike arrats, Set allows you to stoe only unique values.
+//of UNIQUE VALUES. Unlike arrats, Set allows you to stoe only unique values.
 //which means duplicate values are automatically removed. Each value can occur ONCE
 
 
 //Set objects are useful when you need to store a list of unique elements and quickly 
 //check for the existence of a specific value.
+
+const mySet = new Set()
+
+mySet.add("apple")
+mySet.add("banan")
+mySet.add("apple")
+// console.log(mySet.has("banan")); //has method  checks if mySet has "banan"
+// console.log(mySet);
+
+// //delete property in set
+
+// mySet.delete('banan')
+// mySet.clear() // clears set || empty set
+// console.log(mySet); // Set(1) ['apple']
+
+
+for (let item of mySet) {
+    console.log(item);
+}
+
+
+//CHALLENGE SET
+
+
+
+const letters = new Set()
+
+letters.add("a")
+letters.add("b")
+letters.add("c")
+
+for (let letter of letters) {
+    console.log(letter);
+}
