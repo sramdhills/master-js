c29a4b1fce29dc23e2fc7f514842954391f8f5b8
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-07 21:49:54 +0800

    Accessing DOM elements by tagname, class, id , and querySelector

--- 3.DOM/Starter/2. Accessnig the DOM/live-coding/app.js
+++ 3.DOM/Starter/2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,25 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
-// 2. getElementById
-// 3. getElementsByClassName
-// 4. querySelector
-// 5. querySelectorAll
+console.log(document.getElementsByTagName("h1"))
+console.log(document.getElementsByTagName("h1").length)
 
+// 2. getElementById
+console.log(document.getElementById("id-1"))
+
+// 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls-1"))
+
+// 4. querySelector(allows u to select anything by using css selector)
+console.log(document.querySelector("#id-1")) // "" for Tag name. "." for class, "#" ID Selector
+//I Love query Selector BUT, it can only select 1. :< 
+
+// 5. querySelectorAll - that's why I'm here // nodelist
+console.log(document.querySelectorAll(".cls"))
+console.log(document.querySelectorAll("li").length)
 // ---------------------------------------
+
 // Storing data in variables
+const header = document.querySelectorAll("h1")
+console.log(header);
+
+
