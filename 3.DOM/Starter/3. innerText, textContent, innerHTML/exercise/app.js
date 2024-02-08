cc6c11284edfdec0f5300e5c695d7d3922cdb555
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-08 22:49:33 +0800

    Accessing Texts

--- 3.DOM/Starter/3. innerText, textContent, innerHTML/exercise/app.js
+++ 3.DOM/Starter/3. innerText, textContent, innerHTML/exercise/app.js
@@ -1,3 +1,7 @@
 // 1. Check the innerText of "first" element
+const first = document.querySelector(".first")
+console.log(first.innerText);
 // 2. Check the textContent of "second" element
+const second = document.querySelector(".second")
+console.log(second.textContent);
 // 3. Check the innerHTML of "third" element
--- 3.DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
+++ 3.DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,9 +1,11 @@
 // innerText
 const p = document.querySelector("p")
 console.log(p.innerText);
-
-
 // textContent
 console.log(p.textContent);
-
 // innerHTML
+console.log(p.innerHTML);
+
+const header = doc ument.querySelector("h1")
+header.innerText = "TextChanged"
+header.innerHTML = "<del>TextChanged</del>"
