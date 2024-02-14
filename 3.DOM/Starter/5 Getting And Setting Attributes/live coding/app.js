924c8531c0470b70d7eb2ec32d3fe47a64c375c9
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-14 19:45:26 +0800

    as

--- 3.DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ 3.DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -3,3 +3,9 @@
 // type
 // getAttribute(attrName)
 // setAttribute(attrName, value)
+
+const a = document.querySelector("a")
+const input = document.querySelector("input")
+console.log(a.href);
+console.log(input.value);
+console.log(input.type);
