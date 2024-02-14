c57425bfd71fdd729e7c02cdb96e4b885776f8fb
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-14 15:04:47 +0800

    classList. add. remove .toggle

--- 3.DOM/Starter/4. Classlist/app.js
+++ 3.DOM/Starter/4. Classlist/app.js
@@ -2,3 +2,12 @@
 // add()
 // remove()
 // toggle()
+
+const h1 = document.querySelector("h1")
+console.log(h1.classList); // allows to check all the classes attached to the element
+ 
+h1.classList.add("styles") //adds class "styles" to h1
+
+h1.classList.remove("styles")// removes class
+
+h1.classList.toggle('styles')
