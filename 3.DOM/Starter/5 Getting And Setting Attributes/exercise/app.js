002537baadd89f3904e5848ccede6f3d421d87f9
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-15 06:05:13 +0800

    Getting and Setting Attributes. type, value, href

--- 3.DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
+++ 3.DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
@@ -2,3 +2,14 @@
 // 2. Use getAttribute(attrName) to check the attribute.
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+
+const anchor = document.querySelector("a")
+console.log(anchor);
+
+console.log(anchor.getAttribute("href"));
+
+const aTwo =  document.querySelector(".a-two")
+console.log(aTwo.setAttribute("href", "https://www.facebook.com"));
+console.log(aTwo);
+
--- 3.DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ 3.DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -9,3 +9,18 @@ const input = document.querySelector("input")
 console.log(a.href);
 console.log(input.value);
 console.log(input.type);
+
+a.href = "https://www.youtube.com"
+// input.value = "bye"
+input.type = "text"
+input.placeholder = "Please provide a password"
+
+//getAttribute(attrName)
+console.log(input.getAttribute("type"));
+console.log(input.getAttribute("placeholder"));
+
+//setAttribute(attrName, value)
+console.log(input.setAttribute("placeholder", "Email"));
+console.log(input.setAttribute("type","password"));
+
+//CHALLENGE 
