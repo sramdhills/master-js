a51521fcd48b11c101541c582d7435fdbbfaf67e
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-15 08:40:11 +0800

    Parents, Children, Siblings, NextElement,PrevviousElement

---
+++ 3.DOM/Starter/6. Siblings/app.js
@@ -0,0 +1,22 @@
+//Siblings\
+
+// let firstLi = document.querySelector("li")
+// console.log(firstLi.parentElement);
+
+// let ul = document.querySelector("ul")
+// console.log(ul.children);
+// console.log(ul.children[0]);
+// console.log(ul.children[1]);
+// console.log(ul.children[2]);
+// console.log(ul.children[3]);
+
+// console.log(ul.children[0].innerText = "one");
+
+
+// let firstLi = document.querySelector("li")
+// console.log(firstLi.nextElementSibling.textContent); //2 
+// console.log(firstLi.nextElementSibling.nextElementSibling.textContent);//3
+
+let fourthLi = document.querySelector(".fourth")
+console.log(fourthLi.previousElementSibling.textContent);
+console.log(fourthLi.previousElementSibling.previousElementSibling.textContent); 
