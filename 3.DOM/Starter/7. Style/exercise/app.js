a4e20195b8389300e096900f228db17d5a471567
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-16 14:48:42 +0800

    Finishing the Style JS exercise

--- 3.DOM/Starter/7. Style/exercise/app.js
+++ 3.DOM/Starter/7. Style/exercise/app.js
@@ -4,9 +4,15 @@ const p = document.querySelector(".sub-headings");
 const btn = document.querySelector(".btn");
 
 // 1. Proivde "400px" of height to container element
-container.style
+container.style.height = "400px"
+
 // 2. Change the background color of container to "teal"
+container.style.backgroundColor = "teal"
 // 3. Change the color of h1 to "skyblue"
+h1.style.color = "skyblue"
 // 4. Change the fontFamily of paragraph to "sans-serif"
+p.style.fontFamily = "sans-serif"
 // 5. Change the color of paragraph to "white"
+p.style.color = "white"
 // 6. Change the color of btn to pink
+btn.style.color = "pink"
