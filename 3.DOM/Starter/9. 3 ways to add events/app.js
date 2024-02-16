c38b0b960d4251115dcc52470c3c7311dee72871
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-16 21:28:56 +0800

    Best way to write Events

--- 3.DOM/Starter/9. 3 ways to add events/app.js
+++ 3.DOM/Starter/9. 3 ways to add events/app.js
@@ -1,5 +1,19 @@
 // ----------- BAD WAY-----------
-
+const secondBtn = document.querySelector(".second-btn")
+secondBtn.onclick = function() {
+    console.log("Discipline is the way");
+}
 // ----------- GREAT WAY-----------
+// const best = document.querySelector(".best")
 
+// function greetings(){
+//     console.log("hello world");
+// }
+
+// best.addEventListener("click", () => console.log("Yellow"))
 // ----------- Event (e) Object -----------
+
+const para = document. querySelector(".para")
+para.addEventListener("click", (event) => {
+    console.log(event);
+})
