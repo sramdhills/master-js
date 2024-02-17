43b3aa294ce602873d9cd53a83867e83d826237a
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-18 05:12:12 +0800

    Finishing Project 2 - using DOM manipulation, querySelectorAll, forEach,and case

--- 3.DOM/Projects/2. Interchangeable Colors/app.js
+++ 3.DOM/Projects/2. Interchangeable Colors/app.js
@@ -1,40 +1,36 @@
-// Getting Elements From The DOM
-const btns = document.querySelectorAll(".btn");
-const body = document.body;
-// console.log(btns);
+const btns = document.querySelectorAll('.btn')
+const body = document.body
 
-// Iterating over all buttons
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    number = btn.value;
-    changeBackground(number);
-  });
-});
+function changeBackground(colorName) {
+    body.className = ""
 
-function changeBackground(number) {
-  // console.log(number);
-  body.className = "";
-
-  switch (number) {
-    case "purple":
-      body.classList.add("purple");
-      break;
-    case "blue":
-      body.classList.add("blue");
-      break;
-    case "red":
-      body.classList.add("red");
-      break;
-    case "green":
-      body.classList.add("green");
-      break;
-    case "yellow":
-      body.classList.add("yellow");
-      break;
-    case "teal":
-      body.classList.add("teal");
-      break;
-    default:
-      break;
-  }
+    switch(colorName) {
+        case "purple":
+            body.classList.add('purple')
+            break;
+        case "blue":
+            body.classList.add('blue')
+            break;
+        case 'red':
+            body.classList.add('red')
+            break;
+        case 'green':
+            body.classList.add('green')
+            break;
+        case 'yellow':
+            body.classList.add('yellow')
+            break;
+        case 'teal':
+            body.classList.add('teal')
+            break;
+        default:
+            break;
+    }
 }
+
+btns.forEach((btn) => {
+    btn.addEventListener('click',()=>{
+        colorName = btn.value
+        changeBackground(colorName)
+    })
+})
--- 3.DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
+++ 3.DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,22 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+// input.addEventListener('keypress',() => {
+//     console.log("Key pressed");
+// })
+
+// input.addEventListener('keyup',() => {
+//     console.log("Key up");
+// })
+
+// input.addEventListener('keydown',() => {
+//     console.log("Key down");
+// })
+
+//----USEFUL PROPERTIES/ METHODS----//
+input.addEventListener('keypress', e => {
+    // console.log(e.charCode);
+    // console.log(e.code);
+    console.log(e.ctrlKey); //checks if the control key is hold
+    console.log(e.key);
+    console.log(e.shiftKey); // checks if the shift key is hold
+})
