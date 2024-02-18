72b1c071ee9fb49dafe64b89bece97ec3242b513
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-19 06:32:18 +0800

    Interchangeable Colors thru click EventListener, and switch cases

--- 3.DOM/Projects/2. Interchangeable Colors/app.js
+++ 3.DOM/Projects/2. Interchangeable Colors/app.js
@@ -1,27 +1,28 @@
 const btns = document.querySelectorAll('.btn')
 const body = document.body
 
+
 function changeBackground(colorName) {
     body.className = ""
 
-    switch(colorName) {
+    switch(colorName){
         case "purple":
-            body.classList.add('purple')
+            body.classList.add("purple")
             break;
         case "blue":
-            body.classList.add('blue')
+            body.classList.add("blue")
             break;
-        case 'red':
-            body.classList.add('red')
+        case "red":
+            body.classList.add("red")
             break;
-        case 'green':
-            body.classList.add('green')
+        case "green":
+            body.classList.add("green")
             break;
-        case 'yellow':
-            body.classList.add('yellow')
+        case "yellow":
+            body.classList.add("yellow")
             break;
-        case 'teal':
-            body.classList.add('teal')
+        case "teal":
+            body.classList.add("teal")
             break;
         default:
             break;
@@ -29,8 +30,8 @@ function changeBackground(colorName) {
 }
 
 btns.forEach((btn) => {
-    btn.addEventListener('click',()=>{
+    btn.addEventListener('click', () => {
         colorName = btn.value
-        changeBackground(colorName)
+        changeBackground(colorName)    
     })
 })
