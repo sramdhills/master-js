b7a58ee7f4ce7414f9d4fa7dec2053e1caa111bc
Sram D Hills <dhillsbest2000@gmail.com>
2024-02-21 05:49:37 +0800

    Feedback UIProject

--- 3.DOM/Projects/3. Feedback/app.js
+++ 3.DOM/Projects/3. Feedback/app.js
@@ -1,33 +1,32 @@
 // Getting Elements From The DOM
-const ratings = document.querySelectorAll(".ratings");
-const ratingsContainer = document.querySelector(".ratings-container");
-const sendBtn = document.querySelector("#send");
-const panel = document.querySelector("#panel");
+const ratings = document.querySelectorAll(".rating")
+const ratingsContainer = document.querySelector(".ratings-container")
+const sendBtn = document. querySelector("#send")
+const panel = document.querySelector("panel")
 
-let selectedRating = "Satisfied";
+let selectedRating = "Satisfied"
+
 
 // Attaching Event
-ratingsContainer.addEventListener("click", (e) => {
-  if (e.target.parentNode.classList.contains("rating")) {
-    console.log(e.target.classList.contains("rating"));
-    removeActive();
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains("rating")) {
+        // console.log(e.target.parentNode.classList.contains("rating"));
+        removeActive()
+        ratings.target.parentNode.classList.add("active")
+        selectedRating = e.target.nexElementSibling.innerHTML
+    }
+})
 
-    e.target.parentNode.classList.add("active");
-    selectedRating = e.target.nextElementSibling.innerHTML;
-  }
-});
-
-sendBtn.addEventListener("click", () => {
-  panel.innerHTML = `
-    <p class="heart">💖</p>
-    <strong>Thank You! </strong>
+sendBtn.addEventListener('click', () => {
+    panel.innerHTML = `
+    <p class = "heart'> <3/
+    <strong> Thank You! </strong>
     <br>
-    <strong>Feedback: ${selectedRating} </strong>
-  `;
-});
+    <strong> Feedback: ${selectedRating}</strong>
+</p>`
+})
 
 function removeActive() {
-  for (let i = 0; i < ratings.length; i++) {
-    ratings[i].classList.remove("active");
-  }
+    for(let i = 0; i < ratings.length; i++)
+    ratings[i].classList.remove('active')
 }
