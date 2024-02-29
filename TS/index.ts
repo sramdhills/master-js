1eb8179267745fedb6c90e10af3e87a2fcce9377
Sram D Hills <dhillsbest2000@gmail.com>
2024-03-01 07:14:14 +0800

    Passing through arguments with Types

---
+++ TS/index.ts
@@ -0,0 +1,38 @@
+// Boolean Types mini-challenge
+// if the last reviewer is a loyalty User, can you add a star to the end of their name?
+// please do so in the existing function, and make sure to declare what type of 
+// parameters the function takes.
+// : boolean
+
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+let variable = true
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// Solution
+function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '💎' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
