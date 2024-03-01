d3b9ba6581e4ee16b677c96159a62c7c0895df4f
Sram D Hills <dhillsbest2000@gmail.com>
2024-03-01 16:23:59 +0800

    Object Types and Specialized Object Types

---
+++ TS/2.StructuralTypes/ObjectTypes/objects.ts
@@ -0,0 +1,61 @@
+// Object Types
+// In this scrim, based on what we just learnt, can you change userName to firstName, then
+// add an age and lastName to the 'you' Object, making sure to account for it in the Specialised
+// Object for defining its structure.
+
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+const reviewTotalDisplay = document.querySelector('#reviews')
+let isOpen: boolean
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+//type object does not have ; at the end
+const you: {
+    firstName: string
+    lastName: string
+    isReturning: boolean
+    age: number
+} = {
+   firstName: 'Bobby',
+   lastName: 'The First',
+   isReturning: true,
+   age:23,
+}
+
+console.log(you.userName);
+function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
