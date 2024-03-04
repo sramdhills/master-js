d5f00fb6771fb276acd5e3ed34fc2961d1d46936
Sram D Hills <dhillsbest2000@gmail.com>
2024-03-04 08:44:51 +0800

    Speedrun through Any Type where you can infer basically any types to  the object properties, then Union type which just unites types in order to infer 2 or more types, it can also be used for combining values for Literal types, or called Alias type, which passes literal value types through the Alias defined as type typeName = val | val | val

---
+++ TS/2.StructuralTypes/Union Types/utils.ts
@@ -0,0 +1,16 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+import { LoyaltyUser } from './enums'
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
